5af824f6e3b954accf740181b8908a67456cdd7e
Paul Chheang <phalcheat@gmail.com>
2024-05-14 21:41:39 -0400

    Using & ouputting dynamic values [Core Concept]

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+/* Creating an array to store 3 items that will be displayed randomly
+/  under the "React Essentials" section upon page load
+*/
+const randomDescriptions = ['Fundamental', 'Core', 'Crucial'];
+
+function genRandomInt(arrLength){
+  return Math.floor(Math.random() * (arrLength + 1));
+}
+
+
+//Creating custom component "Header"
 function Header() {
+  const description = randomDescriptions[genRandomInt(2)];
+  
   return (
     <header>
     <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+      {/* Displaying dynamic value in the curly braces */}
+      {description} React concepts you will need for almost any app you are
       going to build!
     </p>
   </header>
