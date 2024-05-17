203df9176104bc55e0315ad02aedd49cb3d7cb99
Paul Chheang <phalcheat@gmail.com>
2024-05-16 23:27:08 -0400

    Using object destructure of props object --Short Form

--- src/App.jsx
+++ src/App.jsx
@@ -31,13 +31,14 @@ function Header() {
   )
 }
 
-function CoreConcept(props){
+//Using object destructure of the props object
+function CoreConcept({image, title, description}){
 
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.tile}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   )
 }
