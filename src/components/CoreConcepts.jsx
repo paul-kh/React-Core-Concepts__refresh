81a5f523f5644c3e2c3a0049caa85840d1a3b6f8
Paul Chheang <phalcheat@gmail.com>
2024-05-18 09:30:46 -0400

    Forwarding Props to Wrapped Elements using JavaScript's 'Rest Property' ({...props})

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,16 +1,16 @@
 import { CORE_CONCEPTS } from "../data"; //using {} since CORE_CONCEPTS is a name-export constantr
 import CoreConcept from "../components/CoreConcept";
+import Section from "./Section";
 
 export default function CoreConcepts() {
   return (
-    <section id="core-concepts">
-      <h2>Core Concepts</h2>
+    <Section title="Core Concepts" id="core-concepts">
       <ul>
         {/*COMMON PATTERN: Using array.map() to transform array to JSX code or Renderable Elements */}
         {CORE_CONCEPTS.map((conceptItem) => (
           <CoreConcept key={conceptItem.title} {...conceptItem} />
         ))}
       </ul>
-    </section>
+    </Section>
   );
 }
